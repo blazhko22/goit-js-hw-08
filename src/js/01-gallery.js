c861e689668578f43a065996831b87d94197f9eb
blazhko22 <86318145+blazhko22@users.noreply.github.com>
2021-11-21 12:01:58 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import "simplelightbox/dist/simple-lightbox.min.css";
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const imagesMarkup = makeImageCard(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
+
+function makeImageCard(imgs) {
+  return imgs
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a>
+`;
+    })
+    .join("");
+}
+
+galleryContainer.addEventListener("click", ongalleryContainerClick);
+
+function ongalleryContainerClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+}
+let lightbox = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
