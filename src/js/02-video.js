ffbcdde6deec534a25fe5a748722a842c1eef228
blazhko22 <86318145+blazhko22@users.noreply.github.com>
2021-11-21 21:14:21 +0200

    DZ 82

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(setToLocalStorage, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+function setToLocalStorage(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds.toFixed());
+}
