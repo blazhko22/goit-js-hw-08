cc1092c690aabe992e82e50e2ead0233f90b31e9
blazhko22 <86318145+blazhko22@users.noreply.github.com>
2021-11-22 21:24:18 +0200

    DZ 83

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
 
+const inputLink = document.querySelector('input');
+const textareaLink = document.querySelector('textarea');
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+updateOutput();
+
+form.addEventListener('submit', saveMessage);
+
+function saveMessage(evt) {
+  evt.preventDefault();
+  console.log(localStorage.getItem(LOCALSTORAGE_KEY));
+  evt.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+form.addEventListener('input', throttle(formInput, 500));
+
+function formInput() {
+  localStorage.setItem(
+    LOCALSTORAGE_KEY,
+    JSON.stringify({ email: inputLink.value, text: textareaLink.value }),
+  );
+}
+
+function updateOutput() {
+  const saveText = localStorage.getItem(LOCALSTORAGE_KEY);
+}
